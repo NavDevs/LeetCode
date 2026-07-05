6abf5a158e7a6b15ad5a6fc791eb868127843b2d
Naveen <nas23ise@cmrit.ac.in>
2026-07-05 12:02:37 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (59.78%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        cur =  head
+        prev =None
+        while cur:
+            n = cur.next 
+            cur.next = prev 
+            prev = cur
+            cur = n
+        return prev
+        
+        
+
+        
