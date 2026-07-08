63a8bac37dd7efbe79ecc4c72f88e87c706d8b52
Naveen <nas23ise@cmrit.ac.in>
2026-07-08 15:21:45 +0530

    Time: 2 ms (82.01%) | Memory: 12.5 MB (69.34%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,12 +1,16 @@
 class Solution(object):
     def isValid(self, s):
-        st =[]
-        Map = {")": "(", "]": "[","}": "{", }
+        m = {
+            ")":"(","}":"{","]":"["
+        }
+        st=[]
         for i in s:
             if i in "({[":
                 st.append(i)
-            elif not st or st.pop() != Map[i]: 
-                return False
+            else:
+                if not st or  st.pop() != m[i]:
+                    return False 
+              
         return len(st)==0
         
                                  
