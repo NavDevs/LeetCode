bcc23a5cf795afc0a52531220f39984899187ab8
Naveen <nas23ise@cmrit.ac.in>
2026-07-22 13:39:57 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (93.12%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.py
+++ 113-path-sum-ii/path-sum-ii.py
@@ -1,18 +1,20 @@
 class Solution(object):
     def pathSum(self, root, targetSum):
-        if root is None:
+        if not root:
             return []
 
-       
-        if root.left is None and root.right is None:
+        if root.left is None  and root.right is None:
             if targetSum == root.val:
                 return [[root.val]]
             else:
                 return []
-        ans  =[]
-        for i in self.pathSum(root.left, targetSum - root.val):
+        ans = []
+        
+        for i in self.pathSum(root.left , targetSum-root.val):
             ans.append([root.val]+i)
-        for i in self.pathSum(root.right, targetSum - root.val):
+        for i in self.pathSum(root.right , targetSum-root.val):
             ans.append([root.val]+i)
+        
         return ans 
+         
         
