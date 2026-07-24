2c1d1e8a78f5930514ee3b7e81e9d1185f32de9d
Naveen <nas23ise@cmrit.ac.in>
2026-07-24 20:55:28 +0530

    Time: 15 ms (23.58%) | Memory: 15.2 MB (11.87%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        if len(s) != len(t):
+            return False
+        m1 = {}
+        m2  ={}
+        for i,j in zip(s,t):
+            if i in m1 and m1[i] != j:
+                return False 
+            if j in m2 and m2[j] != i:
+                return False 
+            m1[i] = j 
+            m2[j] = i 
+        return True           
+        
+        
