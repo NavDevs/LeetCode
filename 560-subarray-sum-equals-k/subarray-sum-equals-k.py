4687aa45c70370fea8019d2a2104cb15623594cf
Naveen <nas23ise@cmrit.ac.in>
2026-08-10 18:13:33 +0530

    Time: 38 ms (54.63%) | Memory: 14.8 MB (46.66%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,19 +1,21 @@
 class Solution(object):
     def subarraySum(self, nums, k):
+        c = 0
         freq  = {0:1}
-        c= 0
         p = 0
-
         for i in nums:
-            p += i
 
-            n = p - k
+            p += i 
 
+            n = p - k 
+            
             if n in freq:
                 c += freq[n]
 
             freq[p] = freq.get(p,0) + 1
         
         return c 
+
+
 
              
