48de31759f79439f36fa6bbf559f34afc21adb36
Naveen <nas23ise@cmrit.ac.in>
2026-08-12 16:52:57 +0530

    Time: 33 ms (44.90%) | Memory: 20.4 MB (36.62%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        n  = len(nums)
+        a = [1]*n
+
+        l = 1
+        for i in range(n):
+            a[i] = l
+            l *= nums[i]
+        r=1
+        for i in range(n-1,-1,-1):
+            a[i]*= r
+            r *= nums[i]
+        return a       
