c74d940f89c3d4b84efa3ef56a54eba183169644
Naveen <nas23ise@cmrit.ac.in>
2026-08-15 14:01:26 +0530

    Time: 19 ms (82.48%) | Memory: 12.9 MB (23.88%) - LeetSync

---
+++ 649-dota2-senate/dota2-senate.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def predictPartyVictory(self, senate):
+        senate  = list(senate)
+        R,D = deque(),deque()
+
+        for i,c in enumerate(senate):
+            if c == 'R':
+                R.append(i)
+            else:
+                D.append(i)
+
+        while R and D:
+
+            r  = R.popleft()
+            d = D.popleft()
+
+            if r < d:
+                R.append(len(senate) +d)
+            else:
+                D.append(len(senate) +d)
+        
+        return "Radiant" if R else "Dire"
+
+
+
+
+
+
+
+        
+        
+        
