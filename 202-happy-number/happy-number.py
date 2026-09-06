b68c84cf6d9465a3215babebea7257ab0bc65111
Naveen <nas23ise@cmrit.ac.in>
2026-09-06 16:18:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (55.08%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isHappy(self, n):
+        s  = set()
+
+        while n !=1:
+            if n in s:
+                return False
+            s.add(n)
+            t = 0
+            while n:
+                d  = n % 10
+                t += d*d
+                n //=10
+            n = t
+        return True         
