1f7bc52344f7f91a5f30cde9c3bd778f816d633d
Naveen <nas23ise@cmrit.ac.in>
2026-09-09 22:24:04 +0530

    Time: 82 ms (47.26%) | Memory: 30 MB (14.43%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.py
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.py
@@ -1,31 +1,23 @@
 class Solution(object):
     def minSubarray(self, nums, p):
-        f = {0:-1}
-        total =  sum(nums)
-
+        freq  = {0:-1}
+        total = sum(nums)
         rem  = total % p
-        ans  = len(nums)
-
         if rem  == 0:
             return 0
-        
-        pre = 0
-
+        ans  =len(nums)
+        prefix =0
         for i in range(len(nums)):
+            prefix = (prefix + nums[i]) % p
+            need = (prefix - rem) % p
 
-            pre = (pre+ nums[i]) % p
-
-            n  = (pre -  rem) % p 
-
-            if n in f:
-                ans  = min(ans, i - f[n])
+            if need in freq:
+                ans  = min(ans, i- freq[need])
             
-            f[pre] = i
-
-        if ans == len(nums):
-            return -1
-            
-        return ans 
-
+            freq[prefix] = i
         
+        if ans  == len(nums):
+            return  -1
+        
+        return ans 
         
