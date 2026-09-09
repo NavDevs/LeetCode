364a0d0e5be929bebaf8bbbbf999c2b3028b4bd8
Naveen <nas23ise@cmrit.ac.in>
2026-09-09 18:12:33 +0530

    Time: 27 ms (32.66%) | Memory: 13.3 MB (94.86%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -1,14 +1,9 @@
 class Solution(object):
     def frequencySort(self, s):
+        ans  = ""
 
-        ans =""
-        freq  = {}
-        for i in s:
-            freq[i]  = 1 + freq.get(i,0)
-
-        for i in sorted(freq, key = freq.get, reverse = True ):
-            ans += i *freq[i]
-
-        return ans 
-        
+        freq = Counter(s)
+        for i in sorted(freq, key = freq.get,reverse = True):
+            ans += i*freq[i]
+        return ans
         
