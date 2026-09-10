e20abe8f45c0280d0da54be5ebd819ff06dfe702
Naveen <nas23ise@cmrit.ac.in>
2026-09-10 11:14:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (19.77%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,21 +1,21 @@
 class Solution(object):
     def sortColors(self, nums):
-        l = 0 
-        m = 0 
-        r = len(nums)-1
+        l = 0
+        m = 0
+        r = len(nums) -1
 
-        while m <= r:
+        while m <=r:
             if nums[m] == 0:
-                nums[l], nums[m] = nums[m], nums[l]
-                m+=1
+                nums[l],nums[m] = nums[m], nums[l]
+                m +=1
                 l+=1
-            elif nums[m] ==1:
+            elif nums[m] == 1:
                 m+=1
             else:
-                nums[r], nums[m] = nums[m],nums[r]
-                r-=1
-        return nums
-
+                nums[m],nums[r] = nums[r],nums[m]
+                r -= 1
+            
+        return nums 
         
              
 
