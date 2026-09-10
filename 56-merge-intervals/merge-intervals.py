723ad238b44f766d6588446aa6378a76fa8d504a
Naveen <nas23ise@cmrit.ac.in>
2026-09-10 11:02:47 +0530

    Time: 6 ms (98.86%) | Memory: 15.7 MB (97.63%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,14 +1,14 @@
 class Solution(object):
     def merge(self, intervals):
         intervals.sort()
-        res =[]
+        res = []
+
         for i in intervals:
 
             if not res or res[-1][1] < i[0]:
                 res.append(i)
-
             else:
-                res[-1][1] = max(res[-1][1], i[1] )
+                res[-1][1] = max(res[-1][1], i[1])
         
         return res 
         
