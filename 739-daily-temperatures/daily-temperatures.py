1fe478928aeb650b34a321d625338d3749ebbdf8
Naveen <nas23ise@cmrit.ac.in>
2026-09-11 20:10:03 +0530

    Time: 163 ms (58.92%) | Memory: 26.8 MB (77.47%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -4,8 +4,10 @@ class Solution(object):
         res = [0]*len(temperatures)
        
         for i in range(len(temperatures)):
-            while st and (temperatures[i] > temperatures[st[-1]]):
-                prev = st.pop()
-                res[prev]= i - prev
+            while st and temperatures[i] > temperatures[st[-1]]:
+                index  =st.pop()
+                res[index]=i - index
             st.append(i)
-        return res
+        return res   
+    
+      
