719def34fe6474defe13b6f5856b63603a57d0e5
Naveen <nas23ise@cmrit.ac.in>
2026-09-13 19:12:23 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (30.47%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.py
+++ 101-symmetric-tree/symmetric-tree.py
@@ -7,19 +7,19 @@
 class Solution(object):
     def isSymmetric(self, root):
         def dfs(l,r):
-            if not l and not r :
-                return True
 
-            if not l or not r:
+            if not l and not r:
+                return True
+            if not l or not r :
                 return False
 
-            return ((l.val == r.val)
+            return ((l.val ==  r.val) 
                     and
-                    dfs(l.left,r.right)
-                    and 
-                    dfs(l.right,r.left))
+                    dfs(l.right,r.left)
+                    and
+                    dfs(l.left,r.right))
 
-        return dfs(root.left,root.right)
+        return dfs(root.left,root.right) 
                 
         
                   
