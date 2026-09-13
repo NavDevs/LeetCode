bfb054aa5832a50b25151dc369021991dc39b7c4
Naveen <nas23ise@cmrit.ac.in>
2026-09-13 16:38:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (23.63%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
@@ -1,13 +1,14 @@
 class Solution(object):
     def timeRequiredToBuy(self, tickets, k):
-        res  = 0
+        res  =0
 
         for i in range(len(tickets)):
-            if i <= k:
-                res  += min(tickets[i],tickets[k])
+
+            if i <=k :
+                res +=min(tickets[i],tickets[k])
             else:
-                res += min(tickets[i],tickets[k]-1) 
-        
-        return res 
+                res +=min(tickets[i],tickets[k]-1)
+            
+        return res
 
          
