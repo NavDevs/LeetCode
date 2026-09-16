b6a70981e69880f79d7b4c349af5d25b3d116ef3
Naveen <nas23ise@cmrit.ac.in>
2026-09-16 15:27:37 +0530

    Time: 1281 ms (6.45%) | Memory: 44 MB (7.33%) - LeetSync

--- 502-ipo/ipo.py
+++ 502-ipo/ipo.py
@@ -5,17 +5,19 @@ class Solution(object):
     def findMaximizedCapital(self, k, w, profits, capital):
 
         maxP = []
-        minC = [(c,p) for c,p in zip(capital,profits)]
+
+        minC = [(c,p) for c , p in zip(capital,profits)]
         heapq.heapify(minC)
 
         for _ in range(k):
+
             while minC and minC[0][0] <= w:
-                c, p = heapq.heappop(minC)
-                heapq.heappush(maxP,-1*p)
+                c,p =heapq.heappop(minC)
+                heapq.heappush(maxP,-p)
 
             if not maxP:
                 break
 
-            w += -1 * heapq.heappop(maxP)
+            w += -heapq.heappop(maxP)
 
         return w 
