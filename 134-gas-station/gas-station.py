265588a384e54aaff2e2c2115fba7d8a542c4cd8
Naveen <nas23ise@cmrit.ac.in>
2026-09-16 21:46:17 +0530

    Time: 19 ms (93.92%) | Memory: 18.1 MB (30.01%) - LeetSync

---
+++ 134-gas-station/gas-station.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        if sum(gas) < sum(cost):
+            return -1
+
+        startIndx = 0
+        total = 0
+
+        for i in range(len(gas)):
+
+            total += (gas[i] - cost[i]) 
+
+            if total < 0:
+                total = 0 
+                startIndx = i +1
+            
+        return startIndx
+
+
+        
+        
