5d068ddb1d65fc2803747cb4a32e724d8a038dff
Naveen <nas23ise@cmrit.ac.in>
2026-09-16 13:51:08 +0530

    Time: 135 ms (10.38%) | Memory: 15.8 MB (5.50%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
@@ -5,10 +5,9 @@ class Solution(object):
         for i in matrix:
             for j in i:
                 heapq.heappush(heap,j)
-
+        ans = []
         for _ in range(k):
-            ans=  heapq.heappop(heap)
+            ans = heapq.heappop(heap)
 
         return ans 
-
         
