8cc20eed3c1ca8123ea96dca003308a06e033f80
Naveen <nas23ise@cmrit.ac.in>
2026-09-16 22:44:19 +0530

    Time: 20 ms (36.31%) | Memory: 16 MB (39.58%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -1,17 +1,17 @@
 class Solution(object):
     def findJudge(self, n, trust):
-        ind = [0]*(n+1)
-        o = [0]*(n+1)
+        indegree = [0] * (n+1)
+        outdegree = [0] * (n+1)
 
-        for a,b in trust:
-            ind[a]+=1
-            o[b]+=1
+        for a , b in trust:
+            indegree[a] +=1
+            outdegree[b] +=1
 
         for i in range(1,n+1):
-            if ind[i] == 0 and  o[i] == n -1 :
-                return i 
 
-        return -1 
-        
+            if indegree[i] == 0 and outdegree[i] == n - 1:
+                return i
+
+        return -1             
        
         
