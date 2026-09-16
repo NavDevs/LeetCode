65962a98b4e645244e4c9ccdcd3763dc40b1e3b3
Naveen <nas23ise@cmrit.ac.in>
2026-09-16 12:38:51 +0530

    Time: 6 ms (6.31%) | Memory: 20.2 MB (82.89%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.py
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.py
@@ -2,31 +2,27 @@ class Solution(object):
     def deleteNode(self, root, key):
 
         if not root:
-            return None
+            return root
 
         if key < root.val:
-            root.left = self.deleteNode(root.left, key)
-
+            root.left = self.deleteNode(root.left,key)
         elif key > root.val:
-            root.right = self.deleteNode(root.right, key)
+            root.right = self.deleteNode(root.right,key)
 
         else:
-            # Node found
 
             if not root.left:
                 return root.right
-
-            if not root.right:
+            elif not root.right:
                 return root.left
-
-            # Node has two children
+            
             cur = root.right
-
             while cur.left:
-                cur = cur.left
+                cur  = cur.left
+            root.val = cur.val 
+            root.right = self.deleteNode(root.right,root.val)
 
-            root.val = cur.val
+        return root 
+                         
 
-            root.right = self.deleteNode(root.right, cur.val)
-
-        return root
+        
