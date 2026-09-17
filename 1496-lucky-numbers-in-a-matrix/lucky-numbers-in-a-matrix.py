668f4e7e51fc1f3667203cc65798a1f2c84f3ac3
Naveen <nas23ise@cmrit.ac.in>
2026-09-17 22:15:02 +0530

    Time: 31 ms (10.53%) | Memory: 12.6 MB (61.32%) - LeetSync

---
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def luckyNumbers(self, matrix):
+        res  =[]
+
+        for i in range(len(matrix)):
+            small = min(matrix[i])
+
+            for j in range(len(matrix[0])):
+                if matrix[i][j]:
+                    big = True
+
+                    for k in range(len(matrix)):
+
+                        if matrix[k][j] > small:
+                            big = False
+                            break
+
+                    if big:
+                        res.append(small)
+
+        return res 
+        
