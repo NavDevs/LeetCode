fefe2c78d34b656e4f8305ca72c3a26168d6e0b3
Naveen <nas23ise@cmrit.ac.in>
2026-09-19 14:00:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (19.99%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def spiralOrder(self, matrix):
+        ans = []
+
+        while matrix:
+
+            ans += matrix.pop(0)
+
+            if matrix and matrix[0]:
+                for i in matrix:
+                    ans.append(i.pop())
+
+            if matrix:
+                ans += matrix.pop()[::-1]
+
+            if matrix and matrix[0]:
+                for i in matrix[::-1]:
+                    ans.append(i.pop(0))
+
+        return ans 
+        
