c1ded3d6619a11662a6b6cbe4e8f167f371df142
Naveen <nas23ise@cmrit.ac.in>
2026-09-19 21:05:47 +0530

    Time: 5767 ms (63.35%) | Memory: 12.4 MB (24.60%) - LeetSync

---
+++ 79-word-search/word-search.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def exist(self, board, word):
+        row  = len(board)
+        col = len(board[0])
+
+        def dfs(r,c,i):
+
+            if i == len(word):
+                return True
+
+            if r < 0 or r >= row or c < 0  or c >= col:
+                return False
+
+            if board[r][c] != word[i]:
+                return False
+
+            temp = board[r][c]
+            board[r][c] = '#'
+
+            found = (dfs(r + 1, c, i+1) or
+                    dfs(r - 1, c, i+1) or
+                    dfs(r, c + 1, i+1) or
+                    dfs(r, c - 1, i+1))
+
+            board[r][c] = temp
+
+            return found
+
+        for r in range(row):
+            for c in range(col):
+
+                if dfs(r,c,0):
+                    return True
+
+        return False
+                            
