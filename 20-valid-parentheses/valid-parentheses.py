1d73c9543ace0f1c1b666b05f2e4776b9659406a
Naveen <nas23ise@cmrit.ac.in>
2026-09-23 13:57:18 +0530

    Time: 3 ms (73.79%) | Memory: 12.6 MB (40.96%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -3,12 +3,15 @@ class Solution(object):
         m = {
             ")":"(","}":"{","]":"["
         }
+
         st =[]
+
         for i in s:
             if i in "({[":
                 st.append(i)
             elif not st or st.pop() != m[i]:
                 return False
+                
         return len(st) ==0       
         
         
